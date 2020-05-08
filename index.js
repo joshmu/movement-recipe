d150c44cd8eef130a52b7bcfca2ac7d14e6e2a39
Josh Mu <mu@joshmu.com>
2020-05-08 17:29:14 +1000

    scribbletune added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const recipeMaker = require('./recipeMaker.js')
+const createRecipe = require('./recipe.js')
 
 const chalk = require('chalk')
 const log = console.log
 
-const recipe = recipeMaker({ rounds: 100 })
+const recipe = createRecipe({ rounds: 100 })
+
 log(`define moves:\n${recipe.defineMoves}`)
 log(`# of beats: ${recipe.beats}`)
 log(`# of moves: ${recipe.moves}`)
