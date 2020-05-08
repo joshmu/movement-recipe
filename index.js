cfa0127b4ec242efeaa1dfdf210ebd6cb0a94c65
Josh Mu <mu@joshmu.com>
2020-05-08 16:37:21 +1000

    pattern to 8 counts, # of beats and moves displayed

--- index.js
+++ index.js
@@ -57,20 +57,18 @@ let recipe = [...Array(config.length)].map((x) => {
     .map((move) => [...Array(move.popularity).fill(move.id)])
     .flat()
 
-  // pick one
+  // pick a move ID from the generated list
   const id = list[rand(0, list.length - 1)]
 
-  // reduce popularity
-  dictionary[dictionary.findIndex((move) => move.id === id)].popularity--
-  console.log(dictionary)
-
+  // get the move based on the picked ID
   const move = dictionary.find((move) => move.id === id)
+
+  // reduce popularity of the move
+  dictionary[dictionary.findIndex((move) => move.id === id)].popularity--
+
+  // add to the recipe
   return move
 })
-
-const pattern = recipe
-  .map((move) => `${move.symbol}${'_'.repeat(move.count)}`)
-  .join('')
 
 //////////////////////////////////////////
 // OUTPUT
@@ -81,7 +79,10 @@ console.log(recipe)
 console.log()
 console.log(chalk.blue(JSON.stringify(dictionary, null, 4)))
 console.log()
-console.log(chalk.bold.keyword('orange')(pattern))
+console.log(chalk.bold.keyword('orange')(displayPattern(recipe)))
+console.log()
+console.log(chalk.bold('# of beats: ' + numOfBeats(recipe)))
+console.log(chalk.bold('# of moves: ' + recipe.length))
 
 //////////////////////////////////////////
 // HELPERS
@@ -89,4 +90,20 @@ console.log(chalk.bold.keyword('orange')(pattern))
 
 function rand(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function displayPattern(recipe) {
+  return recipe
+    .map(
+      (move) =>
+        `${move.symbol}${move.count > 1 ? '_'.repeat(move.count - 1) : ''}`
+    )
+    .join('')
+    .split('')
+    .map((char, idx) => (idx % 8 === 7 ? `${char}|` : char))
+    .join('')
+}
+
+function numOfBeats(recipe) {
+  return recipe.reduce((total, move) => total + move.count, 0)
 }
